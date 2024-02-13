018a9f640dca296fe4442b48d09e8284a0293fbc
Zack <zack@gmx.fr>
2024-02-13 13:31:59 +0100

    add server.js

--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/data/playlist', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'data', 'playlist.json'));
+    res.sendFile(path.join(__dirname, '..', 'data', 'playlist.json'));
 });
 
 module.exports = router;
